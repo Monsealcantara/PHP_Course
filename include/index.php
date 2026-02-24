496eb95f7e132c3c291706979ec9c7e13c03306a
Monsealcantara <monserratalco76@gmail.com>
2026-02-23 19:03:59 -0600

    [update] include y require

--- include/index.php
+++ include/index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Principal</title>
 </head>
 <body>
     <?php include_once"./inc/nav.php";?>
     <h1>Página Principal</h1>
-
+    <p>contenido de la página secundaria</p>
+    
     <?php include_once"./inc/footer.php";?>
 </body>
 </html>
--- include/index2.php
+++ include/index2.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Segunda página</title>
 </head>
 <body>
     <?php include_once"./inc/nav.php";?>
-
     <h1>Página Secundaria</h1>
+    <p>contenido de la página secundaria</p>
     <?php include_once"./inc/footer.php";?>
 </body>
 </html>
